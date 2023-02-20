d911bda77f1b049ab2bbba0b375df8b45ab48908
Aakash Gandhi <aakash@cloudwiry.com>
2023-02-20 22:25:34 +0530

    Add Docker Support

--- src/routes/pages/root.ts
+++ src/routes/pages/root.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).header('Content-Type', 'text/html').send(`<h4>Wecome to the Node Server/h4>`);
+  res.status(200).header('Content-Type', 'text/html').send(`<h4>Wecome to the Node Server</h4>`);
 });
 
 export default router;
